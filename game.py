563aecb3f17e65656c3b6e30ef10f97a13ec8941
mckoh <michael.kohlegger@gmail.com>
2026-09-17 19:29:52 +0200

    added game over condition

--- game.py
+++ game.py
@@ -31,6 +31,7 @@ font_big = pg.font.SysFont(None, 50)
 score = 0
 missed = 0
 running = True
+game_over = False
 
 while running:
 
@@ -46,28 +47,47 @@ while running:
     if (keys[pg.K_RIGHT] or keys[pg.K_d]) and player.x < WIDTH-PLAYER_WIDTH:
         player.x += PLAYER_X_SPEED
 
-    # Coin drop
-    coin.y += COIN_Y_SPEED
-    if coin.y > HEIGHT:
-        coin.x = place_coin()
-        coin.y = -COIN_DIAMETER
-        missed += 1
+    if not game_over:
 
-    # Check colliders
-    if player.colliderect(coin):
-        coin.x = place_coin()
-        coin.y = -COIN_DIAMETER
-        score += 1
+        # Coin drop
+        coin.y += COIN_Y_SPEED
+        if coin.y > HEIGHT:
+            coin.x = place_coin()
+            coin.y = -COIN_DIAMETER
+            missed += 1
 
-    # Draw Screen
-    screen.fill(WHITE)
-    pg.draw.rect(screen, BLACK, player)
-    pg.draw.ellipse(screen, COIN_COLOR, coin)
+        # Check colliders
+        if player.colliderect(coin):
+            coin.x = place_coin()
+            coin.y = -COIN_DIAMETER
+            score += 1
 
-    score_text = font_small.render(f"Score: {score}", True, BLACK)
-    missed_text = font_small.render(f"Missed: {missed}", False, BLACK)
-    screen.blit(score_text, (10, 10))
-    screen.blit(missed_text, (10, 40))
+        # Check scores
+        if missed == 3:
+            game_over = True
+
+        # Draw Screen
+        screen.fill(WHITE)
+        pg.draw.rect(screen, BLACK, player)
+        pg.draw.ellipse(screen, COIN_COLOR, coin)
+
+        score_text = font_small.render(f"Score: {score}", True, BLACK)
+        missed_text = font_small.render(f"Missed: {missed}", True, BLACK)
+        screen.blit(score_text, (10, 10))
+        screen.blit(missed_text, (10, 40))
+
+    else:
+        screen.fill(BLACK)
+        gameover_text = font_big.render(f"Game Over!", True, WHITE)
+        restart_text = font_small.render(f"Press 'R' to restart.", True, WHITE)
+        screen.blit(gameover_text, (
+            WIDTH // 2 - gameover_text.get_width() // 2,
+            HEIGHT // 2 - gameover_text.get_height() - 30
+        ))
+        screen.blit(restart_text, (
+            WIDTH // 2 - restart_text.get_width() // 2,
+            HEIGHT // 2 - gameover_text.get_height() +10
+        ))
 
     # Update screen
     pg.display.flip()
