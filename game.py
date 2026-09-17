af1167b619a042d9e55e22cb64e34cf2f684b774
mckoh <michael.kohlegger@gmail.com>
2026-09-17 19:19:53 +0200

    added score text

--- game.py
+++ game.py
@@ -10,6 +10,7 @@ pg.init()
 screen = pg.display.set_mode(SCREEN_SIZE)
 clock = pg.time.Clock()
 
+# Game objects
 player = pg.Rect(
     WIDTH // 2 - PLAYER_WIDTH // 2,
     HEIGHT - PLAYER_HEIGHT - 10,
@@ -24,7 +25,13 @@ coin = pg.Rect(
     COIN_DIAMETER
 )
 
+font_small = pg.font.SysFont(None, 36)
+font_big = pg.font.SysFont(None, 50)
+
+score = 0
+missed = 0
 running = True
+
 while running:
 
     # Eventhandling
@@ -32,7 +39,7 @@ while running:
         if event.type == pg.QUIT:
             running = False
 
-    # Game controlls
+    # Game controls
     keys = pg.key.get_pressed()
     if (keys[pg.K_LEFT] or keys[pg.K_a]) and player.x > 0:
         player.x -= PLAYER_X_SPEED
@@ -44,11 +51,23 @@ while running:
     if coin.y > HEIGHT:
         coin.x = place_coin()
         coin.y = -COIN_DIAMETER
+        missed += 1
+
+    # Check colliders
+    if player.colliderect(coin):
+        coin.x = place_coin()
+        coin.y = -COIN_DIAMETER
+        score += 1
 
     # Draw Screen
     screen.fill(WHITE)
     pg.draw.rect(screen, BLACK, player)
     pg.draw.ellipse(screen, COIN_COLOR, coin)
+
+    score_text = font_small.render(f"Score: {score}", True, BLACK)
+    missed_text = font_small.render(f"Missed: {missed}", False, BLACK)
+    screen.blit(score_text, (10, 10))
+    screen.blit(missed_text, (10, 40))
 
     # Update screen
     pg.display.flip()
